b616a529e8624af24f30724698ffabd4214701d6
Zavista <84294353+Zavista@users.noreply.github.com>
2025-06-12 00:53:59 -0400

    initial backend setup

---
+++ server/src/index.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import * as dynamoose from 'dynamoose';
+import { is } from 'date-fns/locale';
+
+/* Routes Imports */
+
+
+/* Configurations */
+dotenv.config();
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+if (!isProduction) {
+    dynamoose.aws.ddb.local();
+}
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+
+/* Routes */
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+
+/* Server */
+const PORT = process.env.PORT || 8001;
+if (!isProduction) {
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
