769a5f84c6483996ab8910bc1853da548e56c45b
Zavista <84294353+Zavista@users.noreply.github.com>
2025-06-13 21:41:55 -0400

    added course sidebar to layout

--- client/src/app/(dashboard)/layout.tsx
+++ client/src/app/(dashboard)/layout.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import AppSidebar from "@/components/AppSidebar";
 import Loading from "@/components/Loading";
 import Navbar from "@/components/Navbar";
@@ -7,27 +6,47 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 import { cn } from "@/lib/utils";
 import { useUser } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ChaptersSidebar from "./user/courses/[courseId]/ChaptersSidebar";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathName = usePathname();
+  const pathname = usePathname();
   const [courseId, setCourseId] = useState<string | null>(null);
   const { user, isLoaded } = useUser();
+  const isCoursePage = /^\/user\/courses\/[^\/]+(?:\/chapters\/[^\/]+)?$/.test(
+    pathname
+  );
 
-  if (!isLoaded) return <Loading></Loading>;
-  if (!user) return <div>Please sign in to access thhis page</div>;
+  useEffect(() => {
+    if (isCoursePage) {
+      const match = pathname.match(/\/user\/courses\/([^\/]+)/);
+      setCourseId(match ? match[1] : null);
+    } else {
+      setCourseId(null);
+    }
+  }, [isCoursePage, pathname]);
+
+  if (!isLoaded) return <Loading />;
+  if (!user) return <div>Please sign in to access this page.</div>;
 
   return (
     <SidebarProvider>
       <div className="dashboard">
-        <AppSidebar></AppSidebar>
+        <AppSidebar />
         <div className="dashboard__content">
-          <div className={cn("dashboard__main")} style={{ height: "100vh" }}>
-            <Navbar></Navbar>
+          {courseId && <ChaptersSidebar />}
+          <div
+            className={cn(
+              "dashboard__main",
+              isCoursePage && "dashboard__main--not-course"
+            )}
+            style={{ height: "100vh" }}
+          >
+            <Navbar isCoursePage={isCoursePage} />
             <main className="dashboard__body">{children}</main>
           </div>
         </div>
