5c6da684e07ed7b7fd5503e1d10c0c4f6723a02d
Zavista <84294353+Zavista@users.noreply.github.com>
2025-06-14 21:21:06 -0400

    small refactor to index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,33 +5,29 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import * as dynamoose from "dynamoose";
-import { is } from "date-fns/locale";
+import serverless from "serverless-http";
+import seed from "./seed/seedDynamodb";
 import {
   clerkMiddleware,
   createClerkClient,
   requireAuth,
 } from "@clerk/express";
-import seed from "./seed/seedDynamodb";
-import serverless from "serverless-http";
-
-/* Routes Imports */
-import coursesRoutes from "./routes/coursesRoutes";
+/* ROUTE IMPORTS */
+import courseRoutes from "./routes/coursesRoutes";
 import userClerkRoutes from "./routes/userClerkRoutes";
 import transactionRoutes from "./routes/transactionRoutes";
 import userCourseProgressRoutes from "./routes/userCourseProgressRoutes";
 
-/* Configurations */
+/* CONFIGURATIONS */
 dotenv.config();
+const isProduction = process.env.NODE_ENV === "production";
+if (!isProduction) {
+  dynamoose.aws.ddb.local();
+}
 
 export const clerkClient = createClerkClient({
   secretKey: process.env.CLERK_SECRET_KEY,
 });
-
-const isProduction = process.env.NODE_ENV === "production";
-
-if (!isProduction) {
-  dynamoose.aws.ddb.local();
-}
 
 const app = express();
 app.use(express.json());
@@ -43,25 +39,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(clerkMiddleware());
 
-/* Routes */
+/* ROUTES */
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Hello World");
 });
 
-app.use("/courses", coursesRoutes);
+app.use("/courses", courseRoutes);
 app.use("/users/clerk", requireAuth(), userClerkRoutes);
 app.use("/transactions", requireAuth(), transactionRoutes);
 app.use("/users/course-progress", requireAuth(), userCourseProgressRoutes);
 
-/* Server */
-const PORT = process.env.PORT || 8001;
+/* SERVER */
+const port = process.env.PORT || 3000;
 if (!isProduction) {
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
   });
 }
 
-// AWS prod environment
+// aws production environment
 const serverlessApp = serverless(app);
 export const handler = async (event: any, context: any) => {
   if (event.action === "seed") {
